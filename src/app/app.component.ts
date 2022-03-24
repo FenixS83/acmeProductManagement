fb2d064419d65341944b4d5e13b17c7f612d0cc7
Fenix Sampson <Fenix.sampson@gmail.com>
2022-03-24 00:14:04 -0400

    started product-list component build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: '<div><h1>{{pageTitle}}</h1></div>',
+  templateURL: "./app.component.html",
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
 }
