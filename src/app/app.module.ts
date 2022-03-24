8b9b4da9c267b6c13a0ab197182c0b8368747daf
Fenix Sampson <Fenix.sampson@gmail.com>
2022-03-24 02:09:00 -0400

    added images to products as well as formated price to show correctly, also added functionality to show/hide button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
+  // declarations are components that WE declare locally to be acknowledged
   declarations: [
     AppComponent,
     ProductListComponent,
   ],
+  // imports are modules that we import into our application from other sources
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -9,6 +9,8 @@ export class ProductListComponent {
    pageTitle: string = 'Product List';
    imageWidth: number =50;
    imageMargin: number = 2;
+   showImage: boolean = false;
+   listFilter: string = 'cart';
    products: any[] = [
     {
       "productId": 1,
@@ -61,6 +63,12 @@ export class ProductListComponent {
       "imageUrl": "assets/images/xbox-controller.png"
     }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+
+
 
 
   // constructor() { }
