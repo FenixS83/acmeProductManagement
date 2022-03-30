2258a5ad6df11cf9b16fd3b1dc93e96b98ea34bf
Fenix Sampson <Fenix.sampson@gmail.com>
2022-03-30 18:38:18 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(('/build'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join('build/index.html'));
+res.sendFile('build/index.html');
 });
 
 // Start the app by listening on the default Heroku port
